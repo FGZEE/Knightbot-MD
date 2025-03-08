0014de06790c4b435461dde4e4968fcfee4ff9ac
FGZEE <davidmorlly@gmail.com>
2025-03-08 14:52:25 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'knight Bot', // Your name
+  botOwner: 'FGZEE', // Your name
   ownerNumber: '254704400552', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
